833bfdc0f996e7e4b678dc6d130b65c329ac92d0
Nana Adjei Manu <n.k.a.manu06@gmail.com>
2024-06-22 20:14:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r") as f:
 
 setup(
     name="launch_lever",
+    author="Nana Adjei Manu",
     version="1.0.0",
     description="Manage feature toggles(flags) quickly",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    long_description==long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/claeusdev/launch-lever-py"
 )
