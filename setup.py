d9ab46f7de282be7bc9ccc1d44efa1fa64be564e
Nana Adjei Manu <n.k.a.manu06@gmail.com>
2024-06-22 19:58:43 +0100

    Update setup.py
    update setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
     name="launch_lever",
     version="1.0.0",
     description="Manage feature toggles(flags) quickly",
-    packages=setuptools.find_packages("src"),
+    packages=find_packages("src"),
     package_dir={"": "src"},
+    long_description==long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/claeusdev/launch-lever-py"
 )
